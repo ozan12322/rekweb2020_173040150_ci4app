e8f481d6103d497137ac3dcdc2d13eadf165d785
Ozan <bakonong99@gmail.com>
2020-10-18 10:48:21 +0700

    Struktur Folder & Environment

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Coba extends BaseController
+{
+	public function index()
+	{
+		echo "Coba";
+	}
+
+	//--------------------------------------------------------------------
+
+}
