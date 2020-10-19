9d7ff30788b039c31d591233e140d4a2dbe4f126
Ozan <bakonong99@gmail.com>
2020-10-19 19:29:18 +0700

    Model

---
+++ app/Controllers/Komik.php
@@ -0,0 +1,29 @@
+<?php namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+
+	protected $komikModel;
+
+	public function __construct()
+	{
+		$this->komikModel = new KomikModel();
+	}
+
+	public function index()
+	{
+		$komik = $this->komikModel->findAll();
+
+		$data = [
+			'title' => 'DaftarKomik | WebProgrammingUnpas',
+			'komik' => $komik
+		];
+
+		// $komikModel = new \App\Models\KomikModel();
+		return view('komik/index', $data);
+	}
+
+
+}
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -9,6 +9,7 @@
 		      <a class="nav-link active" href="<?= base_url('/'); ?>">Home</a>
 		      <a class="nav-link" href="<?= base_url('/pages/about'); ?>">About</a>
 		      <a class="nav-link" href="/pages/contact">Contact</a>
+		      <a class="nav-link" href="/komik">Komik</a>
 		    </div>
 		  </div>
 		</div>
